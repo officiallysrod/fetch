520615184c425ce50b24a46434270530cc1e0ae9
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-08-31 14:09:51 -0700

    changed message model so that each message will have a sender and a recipient. created new migration file to drop the user_id column from the matches table.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,9 @@
 class Message < ActiveRecord::Base
   
   belongs_to :conversation
-  belongs_to :user
+  belongs_to :sender, class_name: "User", foreign_key: :sender_id
+  belongs_to :recipient, class_name: "User", foreign_key: :recipient_id
 
-  validates :user_id, presence: true
   validates :sender_id, presence: true
   validates :recipient_id, presence: true
   validates :conversation_id, presence: true
