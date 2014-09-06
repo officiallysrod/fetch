4785110f743bbd500cfa9822b9c5f77c3dd22662
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-09-05 19:30:00 -0700

    commented out the serialization_scope in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  serialization_scope :view_context
+  # serialization_scope :view_context
 
   helper_method :current_user
 
