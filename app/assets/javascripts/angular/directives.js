616c1c778924e731d59da2199f7b35656c0b6b1f
nicknish <n.nishiguchi9@gmail.com>
2014-10-05 18:59:40 -0700

    add bg-image directive, fix profile modal from blowing outward

--- app/assets/javascripts/angular/directives.js
+++
@@ -1,8 +0,0 @@
-fetchApp.directive('onLast', function() {
-  var objDiv = document.getElementById("activity-stream");
-      return function(scope, element, attrs) {
-        if (scope.$last){
-          setTimeout(function(){objDiv.scrollTop = objDiv.scrollHeight + 9999;}, 100);
-        }
-    };
-});
