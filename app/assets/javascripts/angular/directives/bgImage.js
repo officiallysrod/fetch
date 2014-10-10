35819fa314743c0aebfe22a0325453f94dd06c10
nicknish <n.nishiguchi9@gmail.com>
2014-10-10 02:07:07 -0700

    add meta tags, messaging svg icon, and performance & design tweaks

--- app/assets/javascripts/angular/directives/bgImage.js
+++ app/assets/javascripts/angular/directives/bgImage.js
@@ -3,7 +3,7 @@ fetchApp.directive('bgImage', function(){
     return function(scope, element, attrs){
         attrs.$observe('bgImage', function(img) {
             element.css({
-                'background-image': 'url(' + img +')'
+                "background-image": "url('" + img +"')"
             });
         });
     };
--- app/assets/javascripts/angular/directives/onLast.js
+++ app/assets/javascripts/angular/directives/onLast.js
@@ -1,8 +1,11 @@
-// fetchApp.directive('onLast', function() {
-//   var objDiv = document.getElementById("activity-stream");
-//       return function(scope, element, attrs) {
-//         if (scope.$last){
-//           setTimeout(function(){objDiv.scrollTop = objDiv.scrollHeight + 9999;}, 100);
-//         }
-//     };
-// });
+// Pushes the activity stream to the bottom of the container
+fetchApp.directive('onLast', function() {
+  var objDiv = document.getElementById("activity-stream");
+      return function(scope, element, attrs) {
+        if (scope.$last){
+          setTimeout(function(){
+          	objDiv.scrollTop = objDiv.scrollHeight + 9999;
+          }, 100);
+        }
+    };
+});
