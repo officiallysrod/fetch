985b0dc7d2e4a5e8d17392fc0703f1bb94f7e88d
nicknish <n.nishiguchi9@gmail.com>
2014-10-11 00:49:54 -0700

    fix some responsiveness issues on landing, begin refactoring swipe functionality into angular directive

--- app/assets/javascripts/angular/controllers/welcomeController.js
+++ app/assets/javascripts/angular/controllers/welcomeController.js
@@ -2,13 +2,16 @@ fetchApp.controller('WelcomeCtrl', ['$scope', function($scope) {
 
   // Trigger Animation ... Refactor into service!
   $scope.triggerAnimate = function(type) {
-    var animationEndEvent = "webkitAnimationEnd mozAnimationEnd animationend";
-    var animate = (type==='like') ? 'animateYes' : 'animateNo';
-    var self = this;
+    var animationEndEvent = "webkitAnimationEnd mozAnimationEnd animationend",
+        animate = (type == 'like') ? 'animateYes' : 'animateNo',
+        self = this,
+        topCard = $('.card').eq(0),
+        topPerson = $('.person').eq(0);
+
     if (!this.blocked) {
       this.blocked = true;
-        $('.card').eq(0).addClass(animate).one(animationEndEvent, function(){
-          $('.person').eq(0).remove();
+        topCard.addClass(animate).one(animationEndEvent, function(){
+          topPerson.remove();
           $(this).remove();
           self.blocked = false;
       });
---
+++ app/assets/javascripts/angular/directives/ngCardSwipe.js
@@ -0,0 +1,27 @@
+// Triggers card swipe animation
+fetchApp.directive('ngCardSwipe', function() {
+	return {
+		restrict: "A",
+		link: function(scope, element, attrs) {
+
+	  		var animationEndEvent = "webkitAnimationEnd mozAnimationEnd animationend",
+	        animate = (type == 'like') ? 'animateYes' : 'animateNo',
+	        self = this,
+	        topCard = $('.card').eq(0),
+	        topPerson = $('.person').eq(0);
+
+				element.bind('click', function(type) {
+			    if (!this.blocked) {
+			    	console.log("something is being called");
+			      this.blocked = true;
+			        topCard.addClass(animate).one(animationEndEvent, function(){
+			          topPerson.remove();
+			          $(this).remove();
+			          self.blocked = false;
+			      });
+			    }
+				});
+		
+		}
+	}
+});
